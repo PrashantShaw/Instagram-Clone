f71afbf30666e91a0f23f6aa05c4177299d547e5
PrashantShaw <prashant.shaw.3154@gmail.com>
2025-01-26 00:28:12 +0530

    chore: databased reset - changed db from sqlite to postgres on neon

--- src/app/(pages)/(home)/_components/HomePage.tsx
+++ src/app/(pages)/(home)/_components/HomePage.tsx
@@ -3,6 +3,7 @@ import Aside from "./Aside";
 import PostItemSkeleton from "@/components/common/skeletons/PostItemSkeleton";
 import Posts from "./Posts";
 
+// TODO: store images to a cloud storage location
 // TODO: add a page to view a particular post
 
 const HomePage = () => {
