82aa168c7f20f9ece969f97430e5f984e8345992
PrashantShaw <prashant.shaw.3154@gmail.com>
2025-02-09 18:47:57 +0530

    feat: toploader bar for page transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/common/ThemeProvider";
+import NextTopLoader, { NextTopLoaderProps } from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Intagram Clone",
@@ -17,9 +18,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const topLoaderProps: NextTopLoaderProps = {
+    color: "#fccc63",
+    height: 3,
+    showSpinner: false,
+  };
+
   return (
     <html lang="en">
       <body className={` antialiased`}>
+        <NextTopLoader {...topLoaderProps} />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Toaster />
